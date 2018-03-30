58452cb416cea1034e26cbda6fd95d18076030d8
Travis Banken <35464088+bankent1@users.noreply.github.com>
2018-03-29 21:37:05 -0700

    Add files via upload

---
+++ src/BinaryMazeBuilderMain.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Travis Banken
+ * BinaryMazeBuilderMain.java
+ * 
+ * java BinaryMazeBuilder infile
+ * 
+ * Notes on Program: 
+ * 
+ * 
+ */
+public class BinaryMazeBuilderMain {
+	
+	public static void main(String[] args) {
+		
+	}
+}
